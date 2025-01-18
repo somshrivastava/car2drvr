2aedc151e10604c95e31ac40d8ded5770f18f644
rishidil <131211616+rishidil@users.noreply.github.com>
2025-01-18 13:48:21 -0500

    rishi changes to about page

--- frontend/src/pages/About/About.jsx
+++ frontend/src/pages/About/About.jsx
@@ -7,7 +7,13 @@ const About = () => {
   return (
     <div>
       <TopNav />
-      <div>what da helllll</div>\
+      <div className = "about" ><p>what the fuck</p>
+      <p>In an ever-changing and unpredictable world, resilience has become one of the most vital traits an individual can possess. The ability to adapt to challenges, recover from setbacks, and grow stronger through adversity is not only essential for personal success but also for the collective progress of society.
+
+Resilience is not about avoiding difficulties; it is about facing them with courage and determination. Life presents us with countless obstacles—be it personal losses, professional failures, or global crises like pandemics and climate change. What sets resilient individuals apart is their mindset. Instead of dwelling on problems, they seek solutions. Instead of giving up, they persevere. This mental strength allows them to navigate uncertainty and emerge stronger than before.
+
+
+</p></div>
     </div>
   );
 };
